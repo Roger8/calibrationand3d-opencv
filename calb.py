057290eb5e5301090b15ab4689fe39051889ed80
Roger8 <viomab@sina.com>
2018-11-23 17:41:11 +0800

    first commit

---
+++ calb.py
@@ -0,0 +1,130 @@
+
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*5,3), np.float32)
+objp[:,:2] = np.mgrid[0:5,0:7].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('imgs/*.jpg')[:15]
+print images
+
+pattern = (5,7)
+for fname in images:
+    print fname
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    print gray.shape
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (5,7),None)
+    print ret
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray,corners,(30,30),(-1,-1),criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (5,7), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(5)
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+print 'ret:', ret
+print 'mtx:', mtx
+print 'dist:', dist
+print 'rvecs:',rvecs
+print 'tvecs:',tvecs
+
+# 
+imx = cv2.imread('imgs/7.jpg')
+h,w,_ = imx.shape
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist, (w,h),1,(w,h))
+#### 1 
+#undistort
+dst = cv2.undistort(imx, mtx, dist, None, newcameramtx)
+# crop
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+#cv2.imwrite('al.png',dst)
+
+#### 2
+# undistort
+mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+dst = cv2.remap(imx,mapx,mapy,cv2.INTER_LINEAR)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('a2.png',dst)
+
+mean_error = 0
+for i in xrange(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print "total error: ", mean_error/len(objpoints)
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    return img
+    
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+
+# draw cube
+axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+def draw(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1,2)
+
+    # draw ground floor in green
+    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+    # draw pillars in blue color
+    for i,j in zip(range(4),range(4,8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+    # draw top layer in red color
+    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+    return img
+
+
+def projet(fname):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (5,7),None)
+
+    if ret == True:
+        corners2 = cv2.cornerSubPix(gray,corners,(30,30),(-1,-1),criteria)
+        print corners2.shape, objp.shape
+
+        # Find the rotation and translation vectors.
+        xxx = cv2.solvePnPRansac(objp, corners2, mtx, dist)
+        retx ,_rvecs, _tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
+
+        # project 3D points to image plane
+        imgpts, jac = cv2.projectPoints(axis, _rvecs, _tvecs, mtx, dist)
+
+        img = draw(img,corners2,imgpts)
+        cv2.imshow('img',img)
+        k = cv2.waitKey(0) & 0xff
+        print k
+        if k == ord('s'):
+            cv2.imwrite(fname[:-4]+'_.png', img)
+        cv2.destroyAllWindows()
+projet('imgs/19.jpg')
